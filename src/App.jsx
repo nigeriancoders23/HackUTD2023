53693720b6d0899e06029d02ecdc29f95b5361e4
Alejandro Appio-Riley <izuappioriley@gmail.com>
2023-11-04 17:38:28 -0500

    Slider (gpt1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,35 @@
-export default function App() {
-  return "Hi"
-}
+import './App.css'
+import React, { useState } from 'react';
+
+const Slider = () => {
+  const [value, setValue] = useState(400);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div className="slider-container">
+      <input
+        type="range"
+        min="0"
+        max="800"
+        step="1"
+        value={value}
+        onChange={handleChange}
+        className="slider"
+      />
+      <input
+        type="number"
+        min="0"
+        max="800"
+        step="1"
+        value={value}
+        onChange={handleChange}
+        className="value-input"
+      />
+    </div>
+  );
+};
+
+export default Slider;
