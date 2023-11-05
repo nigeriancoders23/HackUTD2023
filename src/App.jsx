1f62234a67f140e0ddf9361d6d812410cba912d5
Alejandro Appio-Riley <izuappioriley@gmail.com>
2023-11-04 22:08:10 -0500

    Slider Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import './App.css'
-import React, { useState } from 'react';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import MuiInput from '@mui/material/Input';
 
-const Slider = () => {
-  const [value, setValue] = useState(400);
+const Input = styled(MuiInput)`
+  width: 42px;
+`;
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
+export default function InputSlider() {
+  const [value, setValue] = React.useState(30);
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? 0 : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+    } else if (value > 800) {
+      setValue(800);
+    }
   };
 
   return (
-    <div className="slider-container">
-      <input
-        type="range"
-        min="0"
-        max="800"
-        step="1"
-        value={value}
-        onChange={handleChange}
-        className="slider"
-      />
-      <input
-        type="number"
-        min="0"
-        max="800"
-        step="1"
-        value={value}
-        onChange={handleChange}
-        className="value-input"
-      />
-    </div>
+    <Box sx={{ width: 250 }}>
+      <Typography id="input-slider" gutterBottom>
+        Credit Score
+      </Typography>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs>
+          <Slider
+            value={typeof value === 'number' ? value : 0}
+            onChange={handleSliderChange}
+            max={800}
+            aria-labelledby="input-slider"
+          />
+        </Grid>
+        <Grid item>
+          <Input
+            value={value}
+            size="small"
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 2,
+              min: 0,
+              max: 800,
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Box>
   );
-};
-
-export default Slider;
+}
